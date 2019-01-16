8518cbfa16997a34aba06d1b46f671c0a6dd6d52
RegalWolf <iqbalhasan387@gmail.com>
2019-01-16 09:18:50 +0700

    Hampir fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const home = () => {
-  return (
-    <React.Fragment />
-  );
-};
+import { Typography } from '@material-ui/core';
 
-export default home;
+import FolderIcon from '@material-ui/icons/Folder';
+
+import classes from './Home.module.css';
+
+class Home extends Component {
+  state = {
+    data: [
+      'users',
+      'collections',
+      'classifications',
+      'families',
+      'ages',
+      'acquisitions',
+      'maps',
+      'types',
+      'scales',
+      'islands',
+      'racks',
+      'drawers'
+    ]
+  }
+
+  render() {
+    const datas = this.state.data.map(nama => (
+      <div className={classes.box}>
+        <Link to={'/' + nama} className={classes.data}>
+          <FolderIcon className={classes.icon} />
+          <Typography className={classes.text}>{nama}</Typography>
+        </Link>
+      </div>
+    ));
+
+    return (
+      <div className={classes.home}>
+        {datas}
+      </div>
+    );
+  }
+}
+
+export default Home;
